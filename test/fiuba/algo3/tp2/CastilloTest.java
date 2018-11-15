460f29be21699be8653517c9bea023f2485db10e
gretty94 <44783709+gretty94@users.noreply.github.com>
2018-11-15 05:28:41 -0300

    Castillo pruebas

--- test/fiuba/algo3/tp2/CastilloTest.java
+++ test/fiuba/algo3/tp2/CastilloTest.java
@@ -1,1 +1,14 @@
+package fiuba.algo3.tp2;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CastilloTest {
+    @Test
+    public void castilloCreaArmaDeAsedio(){
+        Castillo castillo = new Castillo();
+        Unidad arma = castillo.crearArmaAsedio();
+        Assert.assertEquals(150, arma.obtenerVida());
+    }
+}
