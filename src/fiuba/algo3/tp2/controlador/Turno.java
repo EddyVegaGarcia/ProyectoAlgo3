8a8dfb20f77db879008673032ede2a3d2b7591a1
gretty <gretty.nazario@gmail.com>
2018-11-22 15:00:57 -0300

    cambios en el modelo en general

---
+++ src/fiuba/algo3/tp2/controlador/Turno.java
@@ -0,0 +1,15 @@
+package fiuba.algo3.tp2.controlador;
+
+import fiuba.algo3.tp2.modelo.*;
+
+public class Turno {
+    Jugador jugador1,jugador2;
+
+    public Turno(Jugador jugador1, Jugador jugador2){
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+    }
+
+
+
+}
