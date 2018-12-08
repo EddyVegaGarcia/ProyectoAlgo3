bf2aa3e7cf4d6b47312c0179f8840a39bc315c4c
Eddy Vega <eavg_93@outlook.com>
2018-12-08 04:48:57 -0300

    Se agrega la excepcion de crear un Castillo y la interfaz de Constructor.

---
+++ src/fiuba/algo3/tp2/modelo/Constructor.java
@@ -0,0 +1,11 @@
+package fiuba.algo3.tp2.modelo;
+
+public interface Constructor {
+
+    void construir(Edificio unEdificio);
+
+    void reparar(Edificio unEdificio);
+
+    boolean estaTrabajando();
+
+}
