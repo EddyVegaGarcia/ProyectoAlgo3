44027066cfe90857d07dd474dfd89b1e3b83738b
Eddy Vega <eavg_93@outlook.com>
2018-12-10 18:56:36 -0300

    Test de Posicion.

--- src/fiuba/algo3/tp2/modelo/Campo/Posicion.java
+++ src/fiuba/algo3/tp2/modelo/Campo/Posicion.java
@@ -5,14 +5,6 @@ public class Posicion {
     private int posicionFila;
     private int posicionColumna;
 
-    /*
-        Posicion(String pos) {
-
-            int[] posicion = this.convertirStringAint(pos);
-            this.x = posicion[0];
-            this.y = posicion[1];
-        }
-    */
     public Posicion(int unaPosicionFila, int unaPosicionColumna) {
         this.posicionFila = unaPosicionFila;
         this.posicionColumna = unaPosicionColumna;
@@ -60,71 +52,4 @@ public class Posicion {
                 }
             }
             return null;
-
-    }
-}
-
-    public boolean esIgualA(String pos) {
-        int[] posicionActual = this.convertirStringAint(pos);
-        return (posicionActual[0] == x) && (posicionActual[1] == y);
-
-    }
-
-    private int[] convertirStringAint(String cadena) {
-
-        int[] resultado = new int[2];
-        String[] posicion = cadena.split(",");
-        resultado[0] = Integer.parseInt(posicion[0]);
-        resultado[1] = Integer.parseInt(posicion[1]);
-        return resultado;
-
-    }
-
-    private String convertirIntAString(int x, int y) {
-        String nuevoX = Integer.toString(x);
-        String nuevoY = Integer.toString(y);
-
-        return nuevoX + "," + nuevoY;
-    }
-
-    public boolean esPosicionValida(String dimension) {
-        int[] dimensionMapa = this.convertirStringAint(dimension);
-        return (x <= dimensionMapa[0]) && (y <= dimensionMapa[1]);
-    }
-
-
-
-
-    public void moverSuperiorDerecha(String dimensionMapa) {
-        String nuevaPosicion = this.convertirIntAString(x-1, y+1);
-        Posicion posicionAMover = new Posicion(nuevaPosicion);
-        if (posicionAMover.esPosicionValida(dimensionMapa)) {
-            this.x = x-1;
-            this.y = y+1;
-        } else {
-            throw new RuntimeException();
-        }
-
-    }
-
-    public void moverInferiorIzquierda(String dimensionMapa) {
-        String nuevaPosicion = this.convertirIntAString(x+1, y-1);
-        Posicion posicionAMover = new Posicion(nuevaPosicion);
-        if (posicionAMover.esPosicionValida(dimensionMapa)) {
-            this.x = x+1;
-            this.y = y-1;
-        } else {
-            throw new RuntimeException();
-        }
-    }
-
-    public void moverInferiorDerecha(String dimensionMapa) {
-        String nuevaPosicion = this.convertirIntAString(x+1, y+1);
-        Posicion posicionAMover = new Posicion(nuevaPosicion);
-        if (posicionAMover.esPosicionValida(dimensionMapa)) {
-            this.x = x+1;
-            this.y = y+1;
-        } else {
-            throw new RuntimeException();
-        }
     }*/
---
+++ test/fiuba/algo3/tp2/PosicionTest.java
@@ -0,0 +1,72 @@
+package fiuba.algo3.tp2;
+
+import fiuba.algo3.tp2.modelo.Campo.Posicion;
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+
+public class PosicionTest {
+
+    @Test
+    public void testCompararPosicionesSimilares() {
+
+        Posicion posicion1 = new Posicion(15,15);
+        Posicion posicion2 = new Posicion(15,15);
+
+        assertTrue(posicion1.compararPosicion(posicion2));
+
+    }
+
+    @Test
+    public void testCompararPosicionesDiferentes() {
+
+        Posicion posicion1 = new Posicion(15,15);
+        Posicion posicion2 = new Posicion(16,16);
+
+        assertFalse(posicion1.compararPosicion(posicion2));
+
+    }
+
+    @Test
+    public void testPosicionEstaEncapsuladaEnLosLimitesMaximosDeOtraPosicion() {
+
+        Posicion posicion1 = new Posicion(20, 20);
+        Posicion posicionEncapsuladora = new Posicion(30,30);
+
+        assertTrue(posicion1.estaContenidaEnDimensiones(posicionEncapsuladora));
+
+    }
+
+    @Test
+    public void testPosicionNoSeEncapsuladaEnLosLimitesMaximosDeOtraPosicion() {
+
+
+        Posicion posicion1 = new Posicion(30, 30);
+        Posicion posicionEncapsuladora = new Posicion(20,20);
+
+        assertFalse(posicion1.estaContenidaEnDimensiones(posicionEncapsuladora));
+
+
+    }
+
+    @Test
+    public void testPosicionEstaEncapuladaEnLosLimitesConTamanioDeterminado() {
+
+        Posicion posicion1 = new Posicion(15,15);
+        Posicion posicion2 = new Posicion(20,20);
+
+        int tamanioDeterminadoDeEncapsulamiento  = 6;
+        assertTrue(posicion2.estaContenidaEn(posicion1, tamanioDeterminadoDeEncapsulamiento));
+
+    }
+
+    @Test
+    public void testTamanioLimiteMaximoParaEncapsularOtrasPosiciones() {
+
+        Posicion posicion1 = new Posicion(20,20);
+
+        int tamanioEsperadoDeEncapsulamiento = 400;
+        assertEquals(tamanioEsperadoDeEncapsulamiento, posicion1.obtenerTamanioLimite());
+
+    }
+}
